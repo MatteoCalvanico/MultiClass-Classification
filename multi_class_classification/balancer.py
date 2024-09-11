3b422dc7677d7e697fcabc2da0b3bd264b379efd
matteo calvanico <matteo.calvanico03@gmail.com>
2024-09-11 19:07:34 +0100

    Update balancer

--- multi_class_classification/balancer.py
+++ multi_class_classification/balancer.py
@@ -2,7 +2,8 @@ import cv2
 import glob
 import sys
 import os
-import matplotlib.pyplot as plt
+import random
+import numpy as np
 
 from pathlib import Path
 from visual_util import ColoredPrint as cp
@@ -35,23 +36,34 @@ class Balancer():
             for img in path.glob('**/*'):
                 cv2Img = cv2.imread(img) # Leggiamo l'immagine con cv2
                 
-                cv2Img = self.__resize(cv2Img) # Facciamo un resize
-                cv2Img = self.__rotate(cv2Img) # Facciamo una rotazione
+                cv2Img = self.__rotate(cv2Img)    # Facciamo una rotazione
                 cv2Img = self.__translate(cv2Img) # Trasliamo l'immagine
                 
                 base_name, ext = os.path.splitext(img) #Otteniamo l'estenzione dell'immagine di partenza (e il nome)
-                cv2.imwrite("balImg_" + str(numImg) + ext, cv2Img)
+                cv2.imwrite(os.path.join(path, "balImg_" + str(numImg) + ext), cv2Img)
                 
-                numImg = numImg - 1
-    
-    def __resize(self, img):
-        pass
+                numImg -= 1
     
     def __rotate(self, img):
-        pass
+        rows, cols = img.shape[0], img.shape[1]
+        
+        image_center = (cols / 2, rows / 2)                 # Punto di rotazione al centro immagine.
+        custom_rotation_angle = random.uniform(0.0, 360.0)  # Angolo di rotazione personalizzato.
+        scale = 1.0                                         # Fattore di scala applicabile all'immagine.
+        
+        transformation_matrix = cv2.getRotationMatrix2D(image_center, custom_rotation_angle, scale) # Costruisco una matrice di rotazione da applicare all'immagine.
+        rotated_img = cv2.warpAffine(img, transformation_matrix, (cols, rows))    # Applico la rotazione all'immagine
+        
+        return rotated_img
     
     def __translate(self, img):
-        pass
+        rows, cols = img.shape[0], img.shape[1]
+        
+        transformation_matrix = np.float32([[1,0,50],[0,1,25]])     # Eseguo la traslazione in x di 50 pixel.
+                                                                    # Eseguo la traslazione in y di 25 pixel.                                                              
+        translated_image = cv2.warpAffine(img, transformation_matrix, (cols, rows)) # Applico la trasformazione all'immagine.
+        
+        return translated_image
         
 
 if __name__ == "__main__":
